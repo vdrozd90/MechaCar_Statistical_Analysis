9d4f42506eb55fc36bc2e673766d02d88a594add
vdrozd90 <85591877+vdrozd90@users.noreply.github.com>
2021-10-11 22:15:32 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,5 +9,21 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 
 #Deliverable 2
 Suspension_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
-suspension_summary <- Suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table
+total_summary <- Suspension_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table
+lot_summary <- Suspension_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 
+#Deliverable 3
+
+#?t.test()
+
+t.test(Suspension_table$PSI, mu=1500)
+
+
+population1 <- subset(Suspension_table, Manufacturing_Lot=='Lot1') 
+t.test(population1$PSI, mu=1500)
+
+population2 <- subset(Suspension_table, Manufacturing_Lot=='Lot2') 
+t.test(x=population2$PSI, mu=1500) 
+
+population3 <- subset(Suspension_table, Manufacturing_Lot=='Lot3') 
+t.test(x=population3$PSI, mu=1500)
